2dd9d37c790229ec2d9eb07fad78b9ca1c77ea00
Dindal <luiztorres.nave@gmail.com>
2015-04-01 11:15:24 -0300

    Lista Duplamente
    Primeiro commit

---
+++ Lista Duplamente/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace listaduplamente
+{
+    public partial class Form1 : Form
+    {
+        ListDindal[] alo = new ListDindal[200];
+
+        public Form1()
+        {
+            int sec = System.DateTime.Now.Second;
+
+            InitializeComponent();
+            alo[0] = new ListDindal(1,null, null);
+
+            for (int i = 1; i < alo.Length; i++)
+            {
+                alo[i] = new ListDindal(i+(2*sec), (i == 0) ? null : alo[i-1], null);
+            }
+
+            for (int i = 1; i < alo.Length;i++ )
+            {
+                alo[i-1].next = alo[i];
+            }
+
+            alo[alo.Length-1].next = alo[0];
+            alo[0].prev = alo[alo.Length - 1];
+
+
+            Console.WriteLine("Prev: "+alo[0].prev.value);
+
+            Console.WriteLine("Next: "+alo[alo.Length-1].value);
+        }
+    }
+}
---
+++ Lista Duplamente/WindowsFormsApplication1/List.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace listaduplamente
+{
+    public class ListDindal
+    {
+        public int value;
+        public ListDindal prev, next;
+
+        public ListDindal(int v, ListDindal p, ListDindal n)
+        {
+            value = v;
+            prev = p;
+            next = n;
+        }
+    }
+}
